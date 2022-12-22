5b4d97eb9481109892f399238bdc667c6548ac35
Sol Fletcher <81700330+SoulFletcher@users.noreply.github.com>
2022-12-22 16:02:13 +0300

    a little buggy

---
+++ cool_text_anim.js
@@ -0,0 +1,27 @@
+var documentFragment = document.createDocumentFragment();
+var my_buttons = document.querySelectorAll('.menu__btn');
+var texts = []
+var arrays = []
+my_buttons.forEach(element => {
+    texts.push(element.innerText)
+    arrays.push(element.innerText.split(''))
+    element.addEventListener('mouseenter', move)
+});
+
+function move(){
+    for (let index = 0; index < my_buttons.length; index++) {
+    my_buttons[index].innerText = '';
+    let c = 0
+    arrays[index].forEach(element => { 
+        c += 190/texts[index].length
+        let a = document.createElement("div");
+        a.classList.add('inletter');
+        a.innerText = `${element}`;
+        a.style.transitionDelay = `${c}ms`
+        my_buttons[index].append(a);
+        }); 
+    }
+    c = 0  
+}
+
+
